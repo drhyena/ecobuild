97c9d8f7bdb615ef8f4959b54d63bfe4a6807ebc
Kevin Paulson <kevinpaulson983@gmail.com>
2026-01-26 09:10:57 +0530

    Add files via upload
    World generation is now done by noise.
    Fixed architectural issues.
    Interactions.py has been expanded upon. Has not been implemented yet.
    Creature simulation loop has been fixed, ever creature goes through each phase simultaneously - no longer giving advantage to creatures that are first in the creature list.
    Encountered an issue where multiple creatures calling AStar pathfinding back-to-back causes freezing due to immense cpu usage.

--- creature.py
+++ creature.py
@@ -6,44 +6,47 @@ from astar import  *
 class Creature:
     def __init__(self, x, y):
         self.x, self.y = x, y
-        self.thirst = 40
+        self.thirst = 100
         self.hunger = 100
         self.target = None
         self.path = []
         self.status = ""
-        self.adjacent_tiles = []
-        self.next_tiles = []
-
-    def update_adjacent_tiles(self, grid_width, grid_height):
-        self.adjacent_tiles = [
-            (self.x + dx, self.y + dy)
-            for dx in range(-10, 11)
-            for dy in range(-10, 11)
-            if 0 <= self.x + dx < grid_width and 0 <= self.y + dy < grid_height
-        ]
-
-        self.next_tiles = [
-            (self.x + dx, self.y + dy)
-            for dx in range(-1, 2)
-            for dy in range(-1, 2)
-            if 0 <= self.x + dx < grid_width and 0 <= self.y + dy < grid_height
-        ]
-
-    def find_closest_veg(self, veg_list, grid_width, grid_height):
-        self.update_adjacent_tiles(grid_width, grid_height)
-        for v in veg_list:
-            if v.status != "alive":
-                continue
-            if (v.v_x, v.v_y) in self.adjacent_tiles:
-                self.target = v
-                return True
-               
-        return False
+        self.perceptive_radius = [
+                                    (dx, dy)
+                                    for dx in range(-5, 6)
+                                    for dy in range(-5, 6)
+                                    if not (dx == 0 and dy == 0)
+                                ]
+        self.perceived_tiles = []
+    
     
 
+   
+    def update_perceived_tiles(self, world):
+        self.perceived_tiles = [
+            (self.x + dx, self.y + dy)
+            for dx, dy in self.perceptive_radius
+            if 0 <= self.x + dx < world.grid_width
+            and 0 <= self.y + dy < world.grid_height
+        ]
+        
     
-    def status_setter(self):
-        self.hunger = self.hunger - 1
+    def drink_water(self):
+            if self.status == "thirsty":
+                print("drinking")
+                self.thirst = 100
+            
+    def eat_veg(self):
+        if self.status in ("hungry", "super hungry"):
+            print("eating")
+            self.hunger = 100
+    
+    def drink_water(self,closest_shore):
+        if (self.x,self.y) == closest_shore:
+            self.thirst == 100
+
+    def update(self,world):
+        self.hunger =self.hunger -1
         self.thirst = self.thirst -3
         if self.hunger < 20:
             self.status = "hungry"
@@ -51,76 +54,54 @@ class Creature:
             self.status = "thirsty"
         else:
             self.status = "wandering"
-        print(self.thirst)
-
-    def status_checker(self,world):
-        if self.status == "wandering":
-            self.wander_randomly(world)
-        if self.status == "hungry":
-            self.find_closest_veg()
-        if self.status == "thirsty":
-            self.target = world.find_closest_shore()
-
-
-    def state_machine(self,grid_width,grid_height,map_grid):
-        if not self.target:
-            self.wander_randomly(grid_width,grid_height,map_grid)
-        else:
-            self.seek_target()
-            self.follow_path()
         
 
+        self.update_perceived_tiles(world)
 
-    def wander_randomly(self, grid_width, grid_height, map_grid,world):
-        dx, dy = random.choice(world.get_neighbours)
-        new_x, new_y = self.x + dx, self.y + dy
+    
+    def status_checker(self,world,veg):
+        if self.status == "hungry":
+            self.target = world.find_closest_veg(veg,self.x,self.y,self.perceived_tiles)
+        elif self.status == "thirsty":
+            self.target = world.find_closest_shore(self.x,self.y,self.perceived_tiles)
+        else:
+            self.target = None
+      
+
+
+    def movement_decider(self,world,screen):
+        if self.target is None:
+            self.wander_randomly(world)
+        else:
+            if not self.path:
+                self.set_path(world)
+                self.wander_randomly(world)           
+               
+            if self.path:
+                self.follow_path(screen,world.tile_size)
+             
+           
+    
+    def wander_randomly(self,world):
+        dx, dy = random.choice(world.get_neighbors(self.x,self.y))
         if world.is_walkable(dx,dy):
-                self.x, self.y = new_x, new_y
-
-    
-    def drink_water(self,closest_shore):
-        if (self.x,self.y) == closest_shore:
-            self.thirst == 100
-
+                self.x, self.y = dx, dy
     
 
 
-
-    def seek_target(self,veg_list,water_list, map_grid, grid_width, grid_height,shore_list
-                ):
-        if self.path:
-            return 
-        if self.status in ("hungry","super hungry"):
-            if self.find_closest_veg(veg_list, grid_width, grid_height):
+    def set_path(self,world):      
                 self.path = astar(
                             (self.x, self.y),
-                            (self.target.v_x, self.target.v_y),
-                            map_grid,
-                            grid_width,
-                            grid_height,
-                    )
-            return     
-        if self.status == "thirsty":
-            print(self.status)
-            print("x")
-            if self.find_closest_shore(shore_list,grid_width,grid_height):
-                print("pathing to")
-                self.path = astar((self.x,self.y), (self.target[0],self.target[1]),
-                                map_grid,
-                                grid_width,
-                                grid_height
-                               )
-                                           
-    def drink_water(self):
-        if self.status == "thirsty":
-            print("drinking")
-            self.thirst = 100
-            
-    def eat_veg(self):
-        if self.status in ("hungry", "super hungry"):
-            print("eating")
-            self.hunger = 100
-
+                            (self.target[0], self.target[1]),
+                            world.map_grid,
+                            world.grid_width,
+                            world.grid_height,
+                    )     
+                if not self.path:
+                     self.target = None
+        
+                               
+                                        
     def follow_path(self, screen, tile_size):
         if self.path:
             # Draw path (visual debug)
--- ecobuild.py
+++ ecobuild.py
@@ -7,48 +7,27 @@ from veg import Veg
 from creature import Creature
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
 clock = pygame.time.Clock()
 
 VEG_SPAWN_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(VEG_SPAWN_EVENT, 10 * 1000)
 
-world = World(GRID_HEIGHT, GRID_WIDTH, TILE_SIZE,NOISE_SCALE, NOISE_OCTAVES,
+world = World(GRID_WIDTH, GRID_HEIGHT, TILE_SIZE,NOISE_SCALE, NOISE_OCTAVES,
     NOISE_PERSISTENCE, NOISE_LACUNARITY,
     WORLD_SEED)
-map_grid = world.map_grid
 
-land_positions = world.return_land()
-water_positions = world.return_water()
-shore_positions = world.return_shore_tiles(land_positions,water_positions)
-
+# Creating Creature and Vegetation objects
 def create_creatures_random(n):
-    return [Creature(*random.choice(land_positions)) for _ in range(n)]
-
-
+    return [Creature(*random.choice(world.return_land())) for _ in range(n)]
 def create_veg_random(n):
-    return [Veg(*random.choice(land_positions)) for _ in range(n)]
-
-
+    return [Veg(*random.choice(world.return_land())) for _ in range(n)]
 
 vege = create_veg_random(10)
 creatures = create_creatures_random(5)
 
-def eating_handler():
-    for ce in creatures:
-        for ve in vege:
-            if (ce.x,ce.y) == (ve.v_x,ve.v_y) and ve.status == "alive" and ce.status in("hungry","super hungry"):
-                ce.eat_veg()
-                ve.confirm_eaten()
 
-def drinking_handler():
-    print("x")
-    for ce in creatures:
-        if  (ce.x,ce.y) == ce.target:
-            ce.drink_water()
-
-
-    
+# MAIN GAME LOOP    
 running = True
 while running:
     screen.fill((0, 0, 0))
@@ -56,24 +35,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == VEG_SPAWN_EVENT and len(vege) < MAX_VEG_COUNT:
-            vege.append(Veg(*random.choice(land_positions)))
-
+            
     world.draw_map(screen)
-    eating_handler()
+    #calling all creature related functions.
     for c in creatures:
-        c.status_setter()
+        c.update(world)
     for c in creatures:   
-        c.status_checker()    
+        c.status_checker(world,vege)    
     for c in creatures:
-        c.state_machine() 
+        c.movement_decider(world,screen)
     for c in creatures:       
         c.draw(screen, TILE_SIZE)
+
 
     for v in vege:
         v.draw(screen, TILE_SIZE)
 
     pygame.display.flip()
-    clock.tick(2)
+    clock.tick(60)
 
 pygame.quit()
