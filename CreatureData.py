6a462b69ee786a39e9d836d47e1597fc6dd08c84
unknown <kevinpaulson983@gmail.com>
2026-07-26 21:22:20 +0530

    Fixed CreatureData classes by allocating the proper variables to the proper data classes. Hunger and thirst threshold -> Genome.

--- CreatureData.py
+++ CreatureData.py
@@ -1,56 +1,36 @@
-
 from dataclasses import dataclass, field
 
 @dataclass
 class Vitals:
-    hunger : float = 100
-    thirst : float = 100
-    
+    hunger: float = 100
+    thirst: float = 100
+
     def decay(self, hunger_rate=1, thirst_rate=2):
         self.hunger -= hunger_rate
         self.thirst -= thirst_rate
-    
-    
-@dataclass
 
+
+@dataclass
 class Targeting:
     target: tuple = None
     path: list = field(default_factory=list)
-    perceived_tiles : list = field(default_factory= list)
+    perceived_tiles: list = field(default_factory=list)
     target_veg: object = None
     target_creature: object = None
     targeted_by: object = None
-    
+
 
 @dataclass
 class Reproduction:
-    reproductive_interval : int = 10000
-    seeking_mate : bool = False
+    reproductive_interval: int = 10000
+    seeking_mate: bool = False
     ready_to_mate: bool = False
-    time_since_last_mating : int = 0
-    
+    time_since_last_mating: int = 0
+
+
 @dataclass
 class Genome:
-    iq : int = None 
-    perceptive_radius : list = field(default_factory= list)
-    
-    
-    
-
-from dataclasses import dataclass
-
-@dataclass
-class Vitals:
-    hunger : float = 100
-    thirst : float = 100
-    
-    def decay(self, hunger_rate=1, thirst_rate=2):
-        self.hunger -= hunger_rate
-        self.thirst -= thirst_rate
-    
-    
-    
-@dataclass
-class Genome:
-    pass
-
+    iq: float = None
+    perceptive_radius: list = field(default_factory=list)
+    hunger_threshold: float = 20
+    thirst_threshold: float = 30
