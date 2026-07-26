5b5753c453eda71494c840255c6b9c8f508d0b5a
unknown <kevinpaulson983@gmail.com>
2026-07-26 21:20:43 +0530

    Fixed Interactionmanager to adhere to the new component based system.

--- interactions.py
+++ interactions.py
@@ -7,32 +7,32 @@ class InteractionSystem:
 
     
     def is_on_target(self,c):
-        return (c.x, c.y) == c.target
+        return (c.x, c.y) == c.targeting.target
 
    
     def check_for_creatures_in_perspective_tiles(self,c1, creature_list):
         perceived_creatures = []
         for c in creature_list:
-            if c is not c1 and (c.x, c.y) in c1.perceived_tiles:
+            if c is not c1 and (c.x, c.y) in c1.targeting.perceived_tiles:
                 perceived_creatures.append(c)
         return perceived_creatures
     
     def if_any_creature_in_perspective_tiles(self,c1,c2):
-        if (c2.x,c2.y) in c1.perceived_tiles:
+        if (c2.x,c2.y) in c1.targeting.perceived_tiles:
             return True
 
     def create_new_creature(self,c):
         pass        
     
     def veg_is_being_targeted(self, c, creature_list):
-        if c.target_veg is None:
+        if c.targeting.target_veg is None:
             return False
 
         for other in creature_list:
             if other == c:
                 continue
 
-            if other.target_veg is not None and other.target_veg == c.target_veg:
+            if other.targeting.target_veg is not None and other.targeting.target_veg == c.targeting.target_veg:
                 return True
 
         return False
@@ -42,13 +42,12 @@ class InteractionSystem:
     def kill_veg(self, veg, veg_list, creature_list):
         if veg in veg_list:
             veg.alive = False
+            veg.claimed_by = None
             veg_list.remove(veg)
 
-        # Remove from any creature targeting it
         for creature in creature_list:
-            if creature.target_veg == veg:
-                creature.target_veg = None
-                
+            if creature.targeting.target_veg == veg:
+                creature.targeting.target_veg = None
     
     def kill_creature(self,creature,creature_list):
         if creature in creature_list:
