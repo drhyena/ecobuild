4a76fad546993b973bb836dcbdd9bbe1c3a64c40
unknown <kevinpaulson983@gmail.com>
2026-08-05 23:54:00 +0530

    minor changes

--- CreatureBehaviors.py
+++ CreatureBehaviors.py
@@ -1,55 +1,56 @@
-
+from astar import astar
+import random
 class Movement:
-    def movement_decider(self, world, screen):
-            if self.targeting.target is None:
-                self.wander_randomly(world)
+    def movement_decider(c, world):
+            if c.targeting.target is None:
+                c.wander_randomly(world)
             else:
-                if not self.targeting.path:
-                    self.set_path(world)
+                if not c.targeting.path:
+                    c.set_path(world)
                     return
     
-                if self.targeting.path:
-                    self.follow_path(screen, world.tile_size)
+                if c.targeting.path:
+                    c.follow_path(screen, world.tile_size)
     
-    def notify_travel(self, target):
+    def notify_travel(c, target):
             """Called by interaction manager to assign a travel target."""
-            if not self.targeting.target:  # don't override if already heading somewhere
-                self.targeting.target = target
-                self.targeting.path = []
+            if not c.targeting.target:  # don't override if already heading somewhere
+                c.targeting.target = target
+                c.targeting.path = []
     
-    def wander_randomly(self, world):
-            dx, dy = random.choice(world.get_neighbors(self.x, self.y))
+    def wander_randomly(c, world):
+            dx, dy = random.choice(world.get_neighbors(c.x, c.y))
             if world.is_walkable(dx, dy):
-                self.prev_x, self.prev_y = self.x, self.y
-                self.x, self.y = dx, dy
+                c.prev_x, c.prev_y = c.x, c.y
+                c.x, c.y = dx, dy
     
-    def set_path(self, world):
-            if self.targeting.target:
-                self.targeting.path = astar(
-                    (self.x, self.y),
-                    (self.targeting.target[0], self.targeting.target[1]),
+    def set_path(c, world):
+            if c.targeting.target:
+                c.targeting.path = astar(
+                    (c.x, c.y),
+                    (c.targeting.target[0], c.targeting.target[1]),
                     world.map_grid,
                     world.grid_width,
                     world.grid_height,
                 )
-                if not self.targeting.path:
-                    self.targeting.target = None
+                if not c.targeting.path:
+                    c.targeting.target = None
     
-    def follow_path(self, screen, tile_size):
-            if self.targeting.path:
+    def follow_path(c, screen, tile_size):
+            if c.targeting.path:
                 points = [
                     (
                         x * tile_size + tile_size // 2,
                         y * tile_size + tile_size // 2,
                     )
-                    for (x, y) in self.targeting.path
+                    for (x, y) in c.targeting.path
                 ]
 
                 if len(points) > 1:
                     pygame.draw.lines(screen, (255, 0, 0), False, points, 2)
     
-                self.prev_x, self.prev_y = self.x, self.y
-                self.x, self.y = self.targeting.path.pop(0)
+                c.prev_x, c.prev_y = c.x, c.y
+                c.x, c.y = c.targeting.path.pop(0)
 
 class PredatorMovement(Movement):
     pass
